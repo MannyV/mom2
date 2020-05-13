78d71a8c821b913e1596dbfc4f6b0f534e7141e0
Sean Moulton <s.moulton772@gmail.com>
2020-05-13 10:54:55 -0400

    added new pages

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import TeamPage from './TeamComponent';
 
 class Main extends Component {
     constructor(props) {
@@ -6,7 +7,25 @@ class Main extends Component {
     }
 
     render() {
+        return (
+            <div>
+
+                <Header />
+                    <Switch>
+                        <Route path='/home' component={HomePage} />
+                        <Route exact path='/calendar' render={() => <TheCalendar events={this.props.events.events} /> } />
+                        <Route exact path='/todolist' component={TodoList}/>
+                        <Route exact path='/about' component={About}/>
+                        <Route exact path='/contact' component={Contact}/>
+                        <Route exact path='/team' component={TeamPage}/>
+                       
+                        <Redirect to='/home' />
+                    </Switch>
+                <Footer />
+
+            </div>
     }
+    
 }
 
 export default Main;
